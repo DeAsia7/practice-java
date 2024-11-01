ca15b7a86e969dd8eda9fcb9d6e4d3f83658471a
DeAsia7 <asiamallory277@gmail.com>
2024-10-31 20:35:25 -0400

    new

---
+++ script.js
@@ -0,0 +1,66 @@
+import {car, motorcycle} from `./vehicle.js`
+
+
+function hideInputs(){
+
+
+    let selector=document.querySelector("select")
+
+let inputpayload= document.getElementById ("inputpayload")
+
+let inputDoors= document.getElementById("inputdoor")
+
+let title1 = document.getElementById("title1") 
+
+let labelDoors = document.getElementById("labelDoors")
+let labelpayload = document.getElementById("labelpayload")
+
+    if(selector.value==="car"){
+inputpayload.style.display= "none"
+labelpayload.style.display= "none"
+
+inputDoors.style.display=" block"
+inputDoors.style.display="inline"
+title1.textcontent = "create car"
+    }
+    if (selector.value==="motorcycle"){
+        inputpayload.style.display= "block"
+inputpayload.style.display = "inline"
+
+        inputDoors.style.display=" none"
+inputDoors.style.display="none"
+        title1.textcontent = "create motorcycle"
+
+    }
+}
+
+function creatvehicle(){
+    event.preventDefault()
+let make = inputmake.value 
+let model = inputmodel.value
+let year= inputyear.value
+let doors = inputdoors.value
+let payload= inputpayload.value
+
+inputtype = inputType.value1
+let pelement= document.querySelector("p")
+
+if (type ==="car"){
+let temp = new car(make, model, year, doors)
+pelement.textContent= tmp.getdetails()
+}
+if (type=="motorcycle"){
+let temp = new motorcycle (make,model, year,payload)
+pelement.textContent= tmp.getdetails()
+}
+
+
+
+
+}
+
+
+window.hideInputs = hideinputs()
+window.createvehicle = createvehicle()
+
+resultcar.innerHTML
---
+++ vehicle.js
@@ -0,0 +1,27 @@
+export class vehicle {
+
+    constructor(make, model, year) {
+        this.make = make
+        this.model = model
+        this.year = year
+
+    }
+    getdetails() {
+        return `the vehicle make ${thi.make} and model ${this.model}`
+    }
+}
+
+export class car extends vehicle {
+
+    constructor(make, model, year, doors)
+    super(make, model, year)
+this.doors = doors
+
+}
+
+export class motorcycle extends vehicle {
+
+    constructor(make, model, year, doors)
+    super(make, model, year)
+
+}
